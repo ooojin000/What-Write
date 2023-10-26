b7e0746cca93dfa25d162a584fa01d96e1f12a0a
김우진 <ooojin523@gmail.com>
2023-10-26 21:31:24 +0900

    feat # ww프로젝트 초기설정

---
+++ src/main/java/com/playdata/whatwrite/WhatwriteApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.whatwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatwriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatwriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playdata/whatwrite/WhatwriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.whatwrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatwriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
